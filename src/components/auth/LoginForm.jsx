cab44bae3e64a8946d05a6fa8197828bc55c73a3
proshantosaha <proshantoll71@gmail.com>
2024-07-18 14:11:30 +0600

    react hooks from use email and password and also use field in input

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,38 +1,60 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import Filed from "../common/Filed";
 
 const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(submitForm)} autoComplete="off">
       <div className="mb-6">
-        <label for="email" className="block mb-2">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="w-full p-3 bg-[#030317] border border-white/20 rounded-md focus:outline-none focus:border-indigo-500"
-        />
+        <Filed label="Email" error={errors.email}>
+          <input
+            {...register("email", { required: "Email Id is Required" })}
+            className=" w-full p-3 bg-[#030317] border border-white/20 rounded-md focus:outline-none focus:border-indigo-500"
+            type="email"
+            id="email"
+            name="email"
+          />
+        </Filed>
       </div>
+
       <div className="mb-6">
-        <label for="password" className="block mb-2">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className="w-full p-3 bg-[#030317] border border-white/20 rounded-md focus:outline-none focus:border-indigo-500"
-        />
+        <Filed label="Password" error={errors.password}>
+          <input
+            {...register("password", {
+              required: "Password Id is Required",
+              minLength: {
+                value: 8,
+                message: "your password must be at least 8 charecters",
+              },
+            })}
+            type="password"
+            id="password"
+            name="password"
+            className=" w-full p-3 bg-[#030317] border border-white/20 rounded-md focus:outline-none focus:border-indigo-500"
+          />
+        </Filed>
       </div>
+
       <div className="mb-6">
-        <button
-          type="submit"
-          className="w-full bg-indigo-600 text-white p-3 rounded-md hover:bg-indigo-700 transition-all duration-200"
-        >
-          Login
-        </button>
+        <Filed>
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white p-3 rounded-md hover:bg-indigo-700 transition-all duration-200"
+          >
+            Login
+          </button>
+        </Filed>
       </div>
+
       <p className="text-center text-white">
         Don't have an account?{" "}
         <a href="./register.html" className="text-indigo-600 hover:underline">
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,17 +5,19 @@ const LoginPage = () => {
   return (
     <main className="flex min-h-screen items-center justify-center  py-8">
       <div className="max-w-[1368px] flex-1">
-        <div className="container grid items-center gap-8 lg:grid-cols-2">
+        <section className="container grid items-center gap-8 lg:grid-cols-2">
           {/* <!-- Login Form into a box center of the page --> */}
           <div>
             <img
-              class="mb-12 max-w-full max-lg:hidden"
+              className="mb-12 max-w-full max-lg:hidden"
               src="./assets/images/auth_illustration.png"
               alt="auth_illustration"
             />
             <div>
-              <h1 class="mb-3 text-4xl font-bold lg:text-[40px]">Blogify</h1>
-              <p class="max-w-[452px] text-gray-600/95 lg:text-lg">
+              <h1 className="mb-3 text-4xl font-bold lg:text-[40px]">
+                Blogify
+              </h1>
+              <p className="max-w-[452px] text-gray-600/95 lg:text-lg">
                 Create a blog website, showing the blog, blog details,
                 reactions, comments and profile.
               </p>
@@ -26,7 +28,7 @@ const LoginPage = () => {
             <h2 className="text-2xl font-bold mb-6">Login</h2>
             <LoginForm />
           </div>
-        </div>
+        </section>
       </div>
     </main>
   );
